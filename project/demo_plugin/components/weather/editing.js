810f94e0ad73c823697c37dfe4b5866df73e982a
yanmao <zhouqimaoyan@163.com>
2014-11-25 19:56:50 +0800

    11.25backup2

--- project/demo_plugin/components/weather/editing.js
+++ project/demo_plugin/components/weather/editing.js
@@ -20,7 +20,7 @@ define(["jquery", "configurableComponent", 'lib/mustache', 'utils/uiHelper', 'ui
             var url = "http://php.weather.sina.com.cn/iframe/index/w_cl.php?code=js&city=" + citytq + "&dfc=3";
             var tq = "";
             var img1, img2;
-            var f, _f, ff;
+            var f, ff;
             $.ajax({
                 url: url,
                 dataType: "script",
@@ -42,40 +42,48 @@ define(["jquery", "configurableComponent", 'lib/mustache', 'utils/uiHelper', 'ui
                     var _tr = $(that.$viewEl).find(".weather_ul").find("li");
                     var _td = $(that.$viewEl).find(".weather_ul_today").find("li");
                     var picurl = "http://mat1.gtimg.com/weather/2014gaiban/";
+                    var DataLength = function(fData) {
+                        var intLength = 0
+                        for (var i = 0; i < fData.length; i++) {
+                            if ((fData.charCodeAt(i) < 0) || (fData.charCodeAt(i) > 255))
+                                intLength = intLength + 2
+                            else
+                                intLength = intLength + 1
+                        }
+                        return intLength;
+                    };
                     for (var key in _w) {
                         if (key == 0) {
-                            $(_td[0]).html( citytq);
+                            $(_td[0]).html("<span>"+citytq+"</span>");
+                            if(DataLength(citytq)>3){that.$viewEl.find(".weather_ul_today li:nth-child(1)").css({"font-size":"20px","height":"40px","line-height":"50px"});}
                                     if (new Date().getHours() > 17) {
-                                        f = _w[key].f1 + "_1.png";
-                                        ff = (_w[key].s1.indexOf("雨")>=0||_w[key].s1.indexOf("雪")>=0)?(_w[key].s1.indexOf("雨")>=0?picurl + "xiaoyu.jpg":picurl + "xue.jpg"):picurl + _w[key].f1 + "_yejian.jpg";
+                                        f = _w[key].f2 + "_1.png";
+                                        ff = (_w[key].s2.indexOf("雨")>=0||_w[key].s2.indexOf("雪")>=0)?(_w[key].s2.indexOf("雨")>=0?picurl + "xiaoyu.jpg":picurl + "xue.jpg"):picurl + _w[key].f2 + "_yejian.jpg";
                                         console.log(ff);
                                         that.$viewEl.find(".weather").css("background-image","url(" + ff +")");
-                                        img1 = "<img width='80px' height='80px' src='http://php.weather.sina.com.cn/images/yb3/180_180/" + f + "'/>";
+                                        img1 = "<img width='70px' height='70px' src='http://php.weather.sina.com.cn/images/yb3/180_180/" + f + "'/>";
                                         $(_td[1]).html( img1);
-                                        $(_td[2]).html( day[date.getDay() + i] +  _w[key].s2);
-                                        $(_td[3]).html( _w[key].s1 +  _w[key].t1 + "℃");
+                                        $(_td[2]).html( day[date.getDay() + i]);
+                                        $(_td[3]).html( _w[key].s2 +  _w[key].t1 + "℃");
                                     }
                                     else {
                                         f = _w[key].f1 + "_0.png";
                                         ff = (_w[key].s1.indexOf("雨")>=0||_w[key].s1.indexOf("雪")>=0)?(_w[key].s1.indexOf("雨")>=0?picurl + "xiaoyu.jpg":picurl + "xue.jpg"):picurl + _w[key].f1 + "_baitian.jpg";
                                         console.log(ff);
                                         that.$viewEl.find(".weather").css("background-image","url(" + ff +")");
-                                        img1 = "<img width='80px' height='80px' src='http://php.weather.sina.com.cn/images/yb3/180_180/" + f + "'/>";
+                                        img1 = "<img width='70px' height='70px' src='http://php.weather.sina.com.cn/images/yb3/180_180/" + f + "'/>";
                                         $(_td[1]).html( img1);
                                         $(_td[2]).html( day[date.getDay() + i] );
                                         $(_td[3]).html( _w[key].s1 +  _w[key].t1 + "℃");
                                     }
-                                    $(_td[4]).html( _w[key].d1 + _w[key].p1 + "级");
+                                    $(_td[4]).html("风力" + _w[key].p1 + "级");
                         } else {
                             f = _w[key].f1 + "_0.png";
-                            _f = _w[key].f2 + "_0.png";
                             if (new Date().getHours() > 17) {
                                 f = _w[key].f1 + "_1.png";
-                                _f = _w[key].f2 + "_1.png";
                             }
                             img1 = "<img width='32px' height='32px' src='http://php.weather.sina.com.cn/images/yb3/78_78/" + f + "'/>";
-                            img2 = "<img width='32px' height='32px' src='http://php.weather.sina.com.cn/images/yb3/78_78/" + _f + "'/>"
-                            tq = day[date.getDay() + i] + " " + img1 + " " + _w[key].s1 +  _w[key].t2 + "℃～" + _w[key].t1 + "℃  " ;
+                            tq = "<span>" + day[date.getDay() + i] + "</span><span>" + img1 + "</span><span>" + _w[key].s1 + "</span><span>" +  _w[key].t2 + "℃～" + _w[key].t1 + "℃</span>" ;
                             $(_tr[i - 1]).html(tq);
                         }
                         i++;
